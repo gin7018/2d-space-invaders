d839704aa629f9e45c434211ff4f57c815b0993b
gin7018 <79063763+gin7018@users.noreply.github.com>
2024-09-28 19:29:30 -0700

    basic intro

---
+++ Game.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class Game {
+
+    public static void main(String[] args) {
+        final JFrame window = new JFrame("SPACE INVADERS");
+        window.setSize(500, 500);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // how to add a piece of graphic
+        // make objects for all entities
+        // add game logic
+        // then boom
+
+
+        window.setVisible(true);
+
+    }
+}
